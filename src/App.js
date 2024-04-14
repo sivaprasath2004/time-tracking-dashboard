3db1535c3b08db857f3b5cd9d298cdb027e3eb33
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-04-14 18:10:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,289 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import avater from "./images/image-jeremy.png";
+import more from "./images/icon-ellipsis.svg";
+import work from "./images/icon-work.svg";
+import play from "./images/icon-play.svg";
+import study from "./images/icon-study.svg";
+import exercise from "./images/icon-exercise.svg";
+import social from "./images/icon-social.svg";
+import Self_care from "./images/icon-self-care.svg";
+import timeTracking from "./data";
 function App() {
+  const [State, setState] = useState({ swiched: "weekly" });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="main">
+        {/* container */}
+
+        <section className="time-tracking">
+          {/* user container */}
+
+          <div className="time-tracking__user-content">
+            <div className="time-tracking__user">
+              <div className="user__avatar-container">
+                <img
+                  className="user__avatar"
+                  src={avater}
+                  alt="Developer avater"
+                />
+              </div>
+              <div className="user__info">
+                <span className="user__report-text">Report for</span>
+                <h1 className="user__name">Jeremy Robson</h1>
+              </div>
+            </div>
+
+            {/* Date and monthly container */}
+
+            <div className="buttons-container">
+              <button
+                className={`btn ${State.swiched === "daily" ? "selected" : ""}`}
+                id="daily-btn"
+                onClick={() =>
+                  setState((pre) => ({ ...pre, swiched: "daily" }))
+                }
+              >
+                Daily
+              </button>
+              <button
+                className={`btn ${
+                  State.swiched === "weekly" ? "selected" : ""
+                }`}
+                id="weekly-btn"
+                onClick={() =>
+                  setState((pre) => ({ ...pre, swiched: "weekly" }))
+                }
+              >
+                Weekly
+              </button>
+              <button
+                className={`btn ${
+                  State.swiched === "monthly" ? "selected" : ""
+                }`}
+                id="monthly-btn"
+                onClick={() =>
+                  setState((pre) => ({ ...pre, swiched: "monthly" }))
+                }
+              >
+                Monthly
+              </button>
+            </div>
+          </div>
+
+          {/* work container */}
+
+          <div className="time-tracking__user-cards">
+            <div className="card" id="work">
+              <div className="card__header">
+                <img className="card__icon" src={work} alt="work" />
+              </div>
+              <div className="card__progress">
+                <div className="card__progress-top">
+                  <h2 className="card__title">Work</h2>
+                  <button className="btn card__btn" title="Show more">
+                    <img src={more} alt="more" />
+                  </button>
+                </div>
+                <div className="card__progress-bottom">
+                  <h3
+                    className="card__actual-time"
+                    id="actual-time"
+                    data-card="Work"
+                    data-time="weekly"
+                  >
+                    {timeTracking[State.swiched][0].current}hrs
+                  </h3>
+                  <span className="card__last-time" id="last-time">
+                    last Day - {timeTracking[State.swiched][0].previous}hrs
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* play container  */}
+            <div className="card" id="play">
+              <div className="card__header">
+                <img className="card__icon" src={play} alt="play" />
+              </div>
+              <div className="card__progress">
+                <div className="card__progress-top">
+                  <h2 className="card__title">Play</h2>
+                  <button className="btn card__btn" title="Show more">
+                    <img src={more} alt="more" />
+                  </button>
+                </div>
+                <div className="card__progress-bottom">
+                  <h3
+                    className="card__actual-time"
+                    id="actual-time"
+                    data-card="Play"
+                    data-time="weekly"
+                  >
+                    {timeTracking[State.swiched][1].current}hrs
+                  </h3>
+                  <span
+                    className="card__last-time"
+                    id="last-time"
+                    data-last="weekly"
+                  >
+                    last Day - {timeTracking[State.swiched][1].previous}hrs
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* Study container */}
+
+            <div className="card" id="study">
+              <div className="card__header">
+                <img className="card__icon" src={study} alt="study" />
+              </div>
+              <div className="card__progress">
+                <div className="card__progress-top">
+                  <h2 className="card__title">Study</h2>
+                  <button className="btn card__btn" title="Show more">
+                    <img src={more} alt="more" />
+                  </button>
+                </div>
+                <div className="card__progress-bottom">
+                  <h3
+                    className="card__actual-time"
+                    id="actual-time"
+                    data-card="Study"
+                    data-time="weekly"
+                  >
+                    {timeTracking[State.swiched][2].current}hrs
+                  </h3>
+                  <span
+                    className="card__last-time"
+                    id="last-time"
+                    data-last="weekly"
+                  >
+                    last Day - {timeTracking[State.swiched][2].previous}hrs
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* Exercise container */}
+
+            <div className="card" id="exercise">
+              <div className="card__header">
+                <img className="card__icon" src={exercise} alt="exercise" />
+              </div>
+              <div className="card__progress">
+                <div className="card__progress-top">
+                  <h2 className="card__title">Exercise</h2>
+                  <button className="btn card__btn" title="Show more">
+                    <img src={more} alt="more" />
+                  </button>
+                </div>
+                <div className="card__progress-bottom">
+                  <h3
+                    className="card__actual-time"
+                    id="actual-time"
+                    data-card="Exercise"
+                    data-time="weekly"
+                  >
+                    {timeTracking[State.swiched][3].current}hrs
+                  </h3>
+                  <span
+                    className="card__last-time"
+                    id="last-time"
+                    data-last="weekly"
+                  >
+                    last Day - {timeTracking[State.swiched][3].previous}hrs
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* social container */}
+
+            <div className="card" id="social">
+              <div className="card__header">
+                <img className="card__icon" src={social} alt="social" />
+              </div>
+              <div className="card__progress">
+                <div className="card__progress-top">
+                  <h2 className="card__title">Social</h2>
+                  <button className="btn card__btn" title="Show more">
+                    <img src={more} alt="more" />
+                  </button>
+                </div>
+                <div className="card__progress-bottom">
+                  <h3
+                    className="card__actual-time"
+                    id="actual-time"
+                    data-card="Social"
+                    data-time="weekly"
+                  >
+                    {timeTracking[State.swiched][4].current}hrs
+                  </h3>
+                  <span
+                    className="card__last-time"
+                    id="last-time"
+                    data-last="weekly"
+                  >
+                    last Day - {timeTracking[State.swiched][4].previous}hrs
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* self care container */}
+
+            <div className="card" id="self-care">
+              <div className="card__header">
+                <img className="card__icon" src={Self_care} alt="self care" />
+              </div>
+              <div className="card__progress">
+                <div className="card__progress-top">
+                  <h2 className="card__title">Self Care</h2>
+                  <button className="btn card__btn" title="Show more">
+                    <img src={more} alt="more" />
+                  </button>
+                </div>
+                <div className="card__progress-bottom">
+                  <h3
+                    className="card__actual-time"
+                    id="actual-time"
+                    data-card="Self Care"
+                    data-time="weekly"
+                  >
+                    {timeTracking[State.swiched][5].current}hrs
+                  </h3>
+                  <span
+                    className="card__last-time"
+                    id="last-time"
+                    data-last="weekly"
+                  >
+                    last Day - {timeTracking[State.swiched][5].previous}hrs
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      {/* Author */}
+
+      <footer>
+        <div className="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw"
+            target="time-tracking-dashboard"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://github.com/sivaprasath2004">sivaprasath2004</a>.
+        </div>
+      </footer>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,98 @@
+const daily = [
+  {
+    title: "work",
+    current: 5,
+    previous: 7,
+  },
+  {
+    title: "play",
+    current: 1,
+    previous: 2,
+  },
+  {
+    title: "Study",
+    current: 0,
+    previous: 1,
+  },
+  {
+    title: "Exercise",
+    current: 1,
+    previous: 1,
+  },
+  {
+    title: "Social",
+    current: 1,
+    previous: 3,
+  },
+  {
+    title: "Self Care",
+    current: 0,
+    previous: 1,
+  },
+];
+const weekly = [
+  {
+    title: "work",
+    current: 32,
+    previous: 36,
+  },
+  {
+    title: "play",
+    current: 10,
+    previous: 8,
+  },
+  {
+    title: "Study",
+    current: 4,
+    previous: 7,
+  },
+  {
+    title: "Exercise",
+    current: 4,
+    previous: 5,
+  },
+  {
+    title: "Social",
+    current: 5,
+    previous: 10,
+  },
+  {
+    title: "Self Care",
+    current: 2,
+    previous: 2,
+  },
+];
+const monthly = [
+  {
+    title: "work",
+    current: 103,
+    previous: 128,
+  },
+  {
+    title: "play",
+    current: 23,
+    previous: 29,
+  },
+  {
+    title: "Study",
+    current: 13,
+    previous: 19,
+  },
+  {
+    title: "Exercise",
+    current: 11,
+    previous: 18,
+  },
+  {
+    title: "Social",
+    current: 21,
+    previous: 23,
+  },
+  {
+    title: "Self Care",
+    current: 7,
+    previous: 11,
+  },
+];
+const timeTracking = { daily, weekly, monthly };
+export default timeTracking;
